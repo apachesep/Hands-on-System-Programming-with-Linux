54c9498037667694f19ac2202f6d7f6c74f2c30d
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2018-01-07 09:58:36 +0530

    A quick code template for C source

---
+++ codetemplate.c
@@ -0,0 +1,40 @@
+/*
+ * ch_:<name>.c
+ * 
+ ***************************************************************
+ * This program is part of the source code released for the book
+ *  "Hands-on System Programming with Linux"
+ *  (c) Author: Kaiwan N Billimoria
+ *  Publisher:  Packt
+ *
+ * From:
+ *  Ch 2 : Virtual Memory
+ ****************************************************************
+ * Brief Description:
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+/*---------------- Globals, Macros ----------------------------*/
+
+
+/*---------------- Typedef's, constants, etc ------------------*/
+
+
+/*---------------- Functions ----------------------------------*/
+
+
+int main(int argc, char **argv)
+{
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s \n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	
+	exit (EXIT_SUCCESS);
+}
+
+/* vi: ts=8 */
