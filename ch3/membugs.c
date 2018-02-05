ec7e5eb2e7bd875a893546a060831094679c45b5
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2018-02-05 09:00:06 +0530

    Ch 3 - initial commit

---
+++ ch3/membugs.c
@@ -0,0 +1,344 @@
+/*
+ * ch3:membugs.c
+ * 
+ ***************************************************************
+ * This program is part of the source code released for the book
+ *  "Hands-on System Programming with Linux"
+ *  (c) Author: Kaiwan N Billimoria
+ *  Publisher:  Packt
+ *
+ * From:
+ *  Ch 3 : Dynamic Memory Management
+ ****************************************************************
+ * Brief Description:
+ *
+ */
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "../common.h"
+
+/*---------------- Globals, Macros ----------------------------*/
+#define MAXVAL     5
+static const size_t BLK_1MB = 1024*1024;
+
+/*---------------- Typedef's, constants, etc ------------------*/
+
+/*---------------- Functions ----------------------------------*/
+
+static void amleaky(unsigned int rep, size_t mem, unsigned int total_reps)
+{
+	char *ptr;
+
+	if ((total_reps <= rep) || (rep % (total_reps*10/100)) == 0) /* only print 10% of the time.. */
+		printf("%s():%6d:malloc(%zu)\n", __FUNCTION__, rep, mem);
+
+	ptr = malloc(mem);
+	if (!ptr)
+		handle_err(EXIT_FAILURE, "%s:%s:%d: malloc failed\n",
+			   __FILE__, __FUNCTION__, __LINE__);
+	memset(ptr, 0, mem);
+
+	/* Bug: no free, leakage */
+}
+
+/* option = 12 : memory leak test case 2: leak in a loop */
+static void leakage_case2(size_t size, unsigned int reps)
+{
+	unsigned int i, threshold = 3*BLK_1MB;
+	double mem_leaked;
+
+	if (reps == 0)
+		reps = 1;
+	mem_leaked = size * reps;
+	printf("%s(): will now leak a total of %.0f bytes (%.2f MB)\n",
+			__FUNCTION__, mem_leaked, mem_leaked/(1024*1024));
+
+	if (mem_leaked >= threshold)
+		system("free|sed '1d'|head -n1");
+	for (i=0; i<reps; i++)
+		amleaky(i, size, reps);
+	if (mem_leaked >= threshold)
+		system("free|sed '1d'|head -n1");
+	printf("\n");
+}
+
+/* option = 11 : memory leak test case 1: simple leak */
+static void leakage_case1(size_t size)
+{
+	printf("%s(): will now leak %zu bytes (%ld MB)\n",
+			__FUNCTION__, size, size/(1024*1024));
+	amleaky(1, size, 1);
+}
+
+/* option = 10 : double-free test case */
+static void doublefree(int cond)
+{
+	char *ptr, *bogus;
+	char name[]="Hands-on Linux Sys Prg";
+	int n=512;
+
+	printf("%s(): cond %d\n", __FUNCTION__, cond);
+	ptr = malloc(n);
+	if (!ptr)
+		handle_err(EXIT_FAILURE, "%s:%s:%d: malloc failed\n",
+			   __FILE__, __FUNCTION__, __LINE__);
+	strncpy(ptr, name, strlen(name));
+	free(ptr);
+
+	if (cond) {
+		bogus = malloc(-1UL); /* will fail! */
+		if (!bogus) {
+			fprintf(stderr, "%s:%s:%d: malloc failed\n",
+			   __FILE__, __FUNCTION__, __LINE__);
+			free(ptr); /* Bug: double-free */
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+/* option =  9 : UAR (use-after-return) test case */
+static void * uar(void)
+{
+	char name[32];
+	
+	memset(name, 0, 32);
+	strncpy(name, "Hands-on Linux Sys Prg", 22);
+
+	return name;
+}
+
+/* option =  8 : UAF (use-after-free) test case */
+static void uaf(void)
+{
+	char *arr, *next;
+	char name[]="Hands-on Linux Sys Prg";
+	int n=512;
+
+	arr = malloc(n);
+	if (!arr)
+		handle_err(EXIT_FAILURE, "%s:%s:%d: malloc failed\n",
+			   __FILE__, __FUNCTION__, __LINE__);
+	memset(arr, 'a', n);
+	arr[n-1]='\0';
+	printf("%s():%d: arr = %p:%.*s\n", __FUNCTION__, __LINE__, arr, 32, arr);
+
+	next = malloc(n);
+	if (!next)
+		handle_err(EXIT_FAILURE, "%s:%s:%d: malloc failed\n",
+			   __FILE__, __FUNCTION__, __LINE__);
+	free(arr);
+
+	strncpy(arr, name, strlen(name)); /* Bug: UAF */
+	printf("%s():%d: arr = %p:%.*s\n", __FUNCTION__, __LINE__, arr, 32, arr);
+	free(next);
+}
+
+/* option =  7 : out-of-bounds : read underflow */
+static void read_underflow(int cond)
+{
+	char *dest, src[] = "abcd56789", *orig;
+
+	printf("%s(): cond %d\n", __FUNCTION__, cond);
+	dest = malloc(25);
+	if (!dest)
+		handle_err(EXIT_FAILURE, "%s:%s:%d: malloc failed\n",
+			   __FILE__, __FUNCTION__, __LINE__);
+	orig = dest;
+
+	strncpy(dest, src, strlen(src));
+	if (cond) { /* Bug, below.. */
+		*(orig-1) = 'x';
+		dest --;
+	}
+	printf(" dest: %s\n", dest);
+
+	free(orig);
+}
+
+/* option =  6 : out-of-bounds : read overflow [on dynamic memory]\n" */
+static void read_overflow_dynmem(void)
+{
+	char *arr;
+
+	arr = malloc(5);
+	if (!arr)
+		handle_err(EXIT_FAILURE, "%s:%s:%d: malloc failed\n",
+			   __FILE__, __FUNCTION__, __LINE__);
+	memset(arr, 'a', 5);
+	/* Bug 1: Steal secrets via a buffer overread.
+	 * Ensure the next few bytes are _not_ NULL.
+	 * Ideally, this should be caught as a bug by the compiler,
+	 * but isn't! (Tools do; seen later).
+	 */
+	arr[5] = 'S'; arr[6] = 'e'; arr[7] = 'c';
+	arr[8] = 'r'; arr[9] = 'e'; arr[10] = 'T';
+	printf("arr = %s\n", arr);
+
+	/* Bug 2, 3: more read buffer overflows */
+	printf("*(arr+100)=%d\n", *(arr + 100));
+	printf("*(arr+10000)=%d\n", *(arr + 10000));
+
+	free(arr);
+}
+
+/* option =  5 : out-of-bounds : read overflow [on compile-time memory] */
+static void read_overflow_compilemem(void)
+{
+	char arr[5], tmp[8];
+
+	memset(arr, 'a', 5);
+	memset(tmp, 't', 8);
+	tmp[7] = '\0';
+
+	printf("arr = %s\n", arr);	/* Bug: read buffer overflow */
+}
+
+/* option =  4 : out-of-bounds : write underflow */
+static void write_underflow(void)
+{
+	char *p = malloc(8);
+
+	if (!p)
+		handle_err(EXIT_FAILURE, "%s:%s:%d: malloc failed\n",
+			   __FILE__, __FUNCTION__, __LINE__);
+
+	p--;
+	strncpy(p, "abcd5678", 8);	/* Bug: write underflow */
+	free(++p);
+}
+
+/* option =  3 : out-of-bounds : write overflow [on dynamic memory] */
+static void write_overflow_dynmem(void)
+{
+	char *dest, src[] = "abcd56789";
+
+	dest = malloc(8);
+	if (!dest)
+		handle_err(EXIT_FAILURE, "%s:%s:%d: malloc failed\n",
+			   __FILE__, __FUNCTION__, __LINE__);
+
+	strcpy(dest, src);	/* Bug: write overflow */
+	free(dest);
+}
+
+/* option =  2 : out-of-bounds : write overflow [on compile-time memory] */
+static void write_overflow_compilemem(void)
+{
+	int i, arr[5];
+
+	for (i = 0; i <= 5; i++) {
+		arr[i] = 100;	/* Bug: 'arr' overflows on i==5,
+				   overwriting part of the 'tmp'
+				   variable - a stack overflow! */
+	}
+}
+
+/* option =  1 : uninitialized var test case */
+static void uninit_var()
+{
+	int x;
+
+	if (x > MAXVAL)
+		printf("true: x=%d\n", x);
+	else
+		printf("false: x=%d\n", x);
+}
+
+static void usage(char *name)
+{
+	fprintf(stderr,
+		"Usage: %s option [ -h | --help]\n"
+		" option =  1 : uninitialized var test case\n"
+		" option =  2 : out-of-bounds : write overflow [on compile-time memory]\n"
+		" option =  3 : out-of-bounds : write overflow [on dynamic memory]\n"
+		" option =  4 : out-of-bounds : write underflow\n"
+		" option =  5 : out-of-bounds : read overflow [on compile-time memory]\n"
+		" option =  6 : out-of-bounds : read overflow [on dynamic memory]\n"
+		" option =  7 : out-of-bounds : read underflow\n"
+		" option =  8 : UAF (use-after-free) test case\n"
+		" option =  9 : UAR (use-after-return) test case\n"
+		" option = 10 : double-free test case\n"
+		" option = 11 : memory leak test case 1: simple leak\n"
+		" option = 12 : memory leak test case 2: leak more (in a loop)\n"
+		" option = 13 : memory leak test case 3: lib API leak\n"
+		"-h | --help : show this help screen\n", name);
+}
+
+static void process_args(int argc, char **argv)
+{
+	void *res;
+
+	if (argc < 2) {
+		usage(argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	if ((argc == 2) && ((strncmp(argv[1], "-h", 2) == 0) ||
+			    (strncmp(argv[1], "--help", 6) == 0))) {
+		usage(argv[0]);
+		exit(EXIT_SUCCESS);
+	}
+
+	if (argc == 2) {
+		switch (atoi(argv[1])) {
+		case 1:
+			uninit_var();
+			break;
+		case 2:
+			write_overflow_compilemem();
+			break;
+		case 3:
+			write_overflow_dynmem();
+			break;
+		case 4:
+			write_underflow();
+			break;
+		case 5:
+			read_overflow_compilemem();
+			break;
+		case 6:
+			read_overflow_dynmem();
+			break;
+		case 7:
+			read_underflow(0);
+			read_underflow(1);
+			break;
+		case 8:
+			uaf();
+			break;
+		case 9:
+			res = uar();
+			printf("res: %s\n", (char *)res);
+			break;
+		case 10:
+			doublefree(0);
+			doublefree(1);
+			break;
+		case 11:
+			leakage_case1(32);
+			leakage_case1(BLK_1MB);
+			break;
+		case 12:
+			leakage_case2(32, 100000);
+			leakage_case2(BLK_1MB, 12);
+			break;
+		case 13:
+			printf("## Leakage test: case 3: lib API\n");
+			//leakage_case3();
+			break;
+		default:
+			usage(argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+int main(int argc, char **argv)
+{
+	process_args(argc, argv);
+	exit(EXIT_SUCCESS);
+}
+
+/* vi: ts=8 */
---
+++ ch3/show_curbrk.c
@@ -0,0 +1,70 @@
+/*
+ * ch3:show_curbrk.c
+ * 
+ ***************************************************************
+ * This program is part of the source code released for the book
+ *  "Hands-on System Programming with Linux"
+ *  (c) Author: Kaiwan N Billimoria
+ *  Publisher:  Packt
+ *
+ * From:
+ *  Ch 3 : Dynamic Memory Management
+ ****************************************************************
+ * Brief Description:
+ * Two cases:
+ * (1) If run without any parameters: displays the current program
+ * break and exits
+ * (2) If passed a parameter - the number of bytes of memory to
+ * dynamically allocate - it does so (with malloc of course), then
+ * prints the heap address returned as well as the original and
+ * current program break.
+ */
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <limits.h>
+#include "../common.h"
+
+/*---------------- Globals, Macros ----------------------------*/
+
+/*---------------- Typedef's, constants, etc ------------------*/
+
+/*---------------- Functions ----------------------------------*/
+
+int main(int argc, char **argv)
+{
+	char *heap_ptr;
+	size_t num = 2048;
+
+	/* No params, just print the current break and exit */
+	if (argc == 1) {
+		printf("Current program break: %p\n", sbrk(0));
+		exit(EXIT_SUCCESS);
+	}
+
+	/* If passed a param - the number of bytes of memory to
+	 * dynamically allocate -, perform a dynamic alloc, then
+	 * print the heap address, the current break and exit.
+	 */
+	num = strtoul(argv[1], 0, 10);
+	if ((errno == ERANGE && num == ULONG_MAX)
+	    || (errno != 0 && num == 0))
+		handle_err(EXIT_FAILURE, "strtoul(%s) failed!\n", argv[1]);
+	if (num >= 128 * 1024)
+		handle_err(EXIT_FAILURE, "%s: pl pass a value < 128 KB\n",
+			   argv[0]);
+
+	printf("Original program break: %p ; ", sbrk(0));
+	heap_ptr = malloc(num);
+	if (!heap_ptr)
+		handle_err(EXIT_FAILURE, "malloc failed!");
+	printf("malloc(%lu) = %16p ; curr break = %16p\n",
+	       num, heap_ptr, sbrk(0));
+	free(heap_ptr);
+
+	exit(EXIT_SUCCESS);
+}
+
+/* vi: ts=8 */
