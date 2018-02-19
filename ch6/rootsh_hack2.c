09afc74ad8a21eeed3d99168f70573e42c3a6bf4
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2018-02-19 17:53:02 +0530

    Initial Commit

---
+++ ch6/rootsh_hack2.c
@@ -0,0 +1,45 @@
+/*
+ * ch7:rootsh_hack2.c
+ * 
+ ***************************************************************
+ * This program is part of the source code released for the book
+ *  "Hands-on System Programming with Linux"
+ *  (c) Author: Kaiwan N Billimoria
+ *  Publisher:  Packt
+ *
+ * From:
+ *  Ch 7 : Process Credentials
+ ****************************************************************
+ * Brief Description:
+ *
+ */
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include "../common.h"
+
+/*---------------- Globals, Macros ----------------------------*/
+
+
+/*---------------- Typedef's, constants, etc ------------------*/
+
+
+/*---------------- Functions ----------------------------------*/
+
+
+int main(int argc, char **argv)
+{
+	/* Become root */
+	if (setuid(0) == -1)
+		WARN("setuid(0) failed!\n");
+
+	/* Now just spawn a shell;
+	 * <i>Evil Laugh</i>, we're now root!
+	 */
+	system("/bin/sh");
+	exit (EXIT_SUCCESS);
+}
+
+/* vi: ts=8 */
