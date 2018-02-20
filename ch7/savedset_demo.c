189aee17f68a8511b78949e5929e3e352226c261
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2018-02-20 09:18:38 +0530

    add printf's with "time t0.." type of message minor cleanups

--- ch7/savedset_demo.c
+++ ch7/savedset_demo.c
@@ -1,5 +1,5 @@
 /*
- * ch_:savedset_demo.c
+ * ch7:savedset_demo.c
  * 
  ***************************************************************
  * This program is part of the source code released for the book
@@ -8,7 +8,7 @@
  *  Publisher:  Packt
  *
  * From:
- *  Ch 6 : Process Credentials
+ *  Ch 7 : Process Credentials
  ****************************************************************
  * Brief Description:
  *
@@ -35,9 +35,9 @@
 
 int main(int argc, char **argv)
 {
-	uid_t savedsetuid;
+	uid_t saved_setuid;
 
-	printf("1. At Startup:\n");
+	printf("t0: Init:\n");
 	SHOW_CREDS();
 	if (0 != geteuid())
 		FATAL("Not a setuid-root executable,"
@@ -49,23 +49,23 @@ int main(int argc, char **argv)
 	printf(" Ok, we're effectively running as root! (EUID==0)\n");
 
 	/* Save the EUID, in effect the "saved set UID", so that
-	 * we can switch back anf forth
+	 * we can switch back and forth
 	 */
-	savedsetuid = geteuid();
+	saved_setuid = geteuid();
 
-	printf("2. Becoming my original self!\n");
+	printf("t1: Becoming my original self!\n");
 	if (seteuid(getuid()) == -1)
 		FATAL("seteuid() step 2 failed!\n");
 	SHOW_CREDS();
 
-	printf("3. Switching to privileged state now...\n");
-	if (seteuid(savedsetuid) == -1)
+	printf("t2: Switching to privileged state now...\n");
+	if (seteuid(saved_setuid) == -1)
 		FATAL("seteuid() step 3 failed!\n");
 	SHOW_CREDS();
 	if (0 == geteuid())
 		printf(" Yup, we're root again!\n");
 
-	printf("4. Switching back to unprivileged state now ...\n");
+	printf("t3: Switching back to unprivileged state now ...\n");
 	if (seteuid(getuid()) == -1)
 		FATAL("seteuid() step 4 failed!\n");
 	SHOW_CREDS();
