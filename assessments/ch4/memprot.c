1ce43d03cad21c7e896f9a3ba6d8c65cd81816a9
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2018-02-21 18:48:11 +0530

    Again just a little code cleanup

--- assessments/ch4/memprot.c
+++ assessments/ch4/memprot.c
@@ -35,20 +35,21 @@ int main(int argc, char **argv)
 	/* POSIX wants page-aligned memory for mprotect(2) */
 	posix_memalign(&ptr, gPgsz, 2*gPgsz);
 	if (!ptr) {
-		printf("posix_memalign(for %zu bytes) failed\n", 4*gPgsz);
+		fprintf(stderr,"posix_memalign(for %u bytes) failed\n", 4*gPgsz);
 		exit(1);
 	}
 
 	//--- Test case 1: set page 0 to [r-x]
 	printf("Attempting to set memory protections of %p to [r-x]...\n", ptr);
 	if (mprotect((void *)ptr, gPgsz, PROT_READ|PROT_EXEC) == -1)
-		printf(" *** mprotect(r-x) failed\n");
+		fprintf(stderr," *** mprotect(r-x) failed\n");
 	else
 		printf(" [r-x] successful\n");
-	/* Find that the above test case fails! Why?
-	 * It's really to do with the kernel security modules disabling
-	 * it. On Fedora, it's SELinux, on Ubuntu it's usually AppArmor
-	 * responsible. So, either disable the kernel LSM's or run on a 
+	/* Find that the above test case might fail! Why?
+	 * It's really to do with kernel security modules disabling
+	 * the mprotect(). On Fedora, the LSM is usually SELinux,
+	 * on Ubuntu it's usually AppArmor responsible. 
+	 * So, either disable the kernel LSM's or run on a 
 	 * system with no kernel security modules installed.
 	 * It should then work.
 	 */
@@ -56,7 +57,7 @@ int main(int argc, char **argv)
 	//--- Test case 2: set page 1 to [r--]
 	printf("Attempting to set memory protections of %p to [r--]...\n", ptr+gPgsz);
 	if (mprotect((void *)ptr+gPgsz, gPgsz, PROT_READ) == -1)
-		printf(" *** mprotect(r--) failed\n");
+		fprintf(stderr," *** mprotect(r--) failed\n");
 	else
 		printf(" [r--] successful\n");
 	// Succeeds!
