4ddfb70e43d9122990cecdb2faf14d86ad073cf5
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2018-03-12 11:36:44 +0530

    Replace the word 'option' with 'test case'

--- ch5/membugs.c
+++ ch5/membugs.c
@@ -44,7 +44,7 @@ static void silly_getpath(char **ptr)
 		WARN("getenv failed\n");
 }
 
-/* option = 13 : memory leak test case 3: "lib" API leak */
+/* test case = 13 : memory leak test case 3: "lib" API leak */
 static void leakage_case3(int cond)
 {
 	char *mypath=NULL;
@@ -72,7 +72,7 @@ static void amleaky(size_t mem)
 	/* Bug: no free, leakage */
 }
 
-/* option = 12 : memory leak test case 2: leak in a loop */
+/* test case 12 : memory leak test case 2: leak in a loop */
 static void leakage_case2(size_t size, unsigned int reps)
 {
 	unsigned int i, threshold = 3*BLK_1MB;
@@ -100,7 +100,7 @@ static void leakage_case2(size_t size, unsigned int reps)
 	printf("\n");
 }
 
-/* option = 11 : memory leak test case 1: simple leak */
+/* test case 11 : memory leak test case 1: simple leak */
 static void leakage_case1(size_t size)
 {
 	printf("%s(): will now leak %zu bytes (%ld MB)\n",
@@ -108,7 +108,7 @@ static void leakage_case1(size_t size)
 	amleaky(size);
 }
 
-/* option = 10 : double-free test case */
+/* test case 10 : double-free test case */
 static void doublefree(int cond)
 {
 	char *ptr, *bogus;
@@ -133,7 +133,7 @@ static void doublefree(int cond)
 	}
 }
 
-/* option =  9 : UAR (use-after-return) test case */
+/* test case 9 : UAR (use-after-return) test case */
 static void * uar(void)
 {
 	char name[32];
@@ -144,7 +144,7 @@ static void * uar(void)
 	return name;
 }
 
-/* option =  8 : UAF (use-after-free) test case */
+/* test case 8 : UAF (use-after-free) test case */
 static void uaf(void)
 {
 	char *arr, *next;
@@ -168,7 +168,7 @@ static void uaf(void)
 	free(next);
 }
 
-/* option =  7 : out-of-bounds : read underflow */
+/* test case 7 : out-of-bounds : read underflow */
 static void read_underflow(int cond)
 {
 	char *dest, src[] = "abcd56789", *orig;
@@ -189,7 +189,7 @@ static void read_underflow(int cond)
 	free(orig);
 }
 
-/* option =  6 : out-of-bounds : read overflow [on dynamic memory] */
+/* test case 6 : out-of-bounds : read overflow [on dynamic memory] */
 static void read_overflow_dynmem(void)
 {
 	char *arr;
@@ -214,7 +214,7 @@ static void read_overflow_dynmem(void)
 	free(arr);
 }
 
-/* option =  5 : out-of-bounds : read overflow [on compile-time memory] */
+/* test case 5 : out-of-bounds : read overflow [on compile-time memory] */
 static void read_overflow_compilemem(void)
 {
 	char arr[5], tmp[8];
@@ -226,7 +226,7 @@ static void read_overflow_compilemem(void)
 	printf("arr = %s\n", arr);	/* Bug: read buffer overflow */
 }
 
-/* option =  4 : out-of-bounds : write underflow */
+/* test case 4 : out-of-bounds : write underflow */
 static void write_underflow(void)
 {
 	char *p = malloc(8);
@@ -239,7 +239,7 @@ static void write_underflow(void)
 	free(++p);
 }
 
-/* option =  3 : out-of-bounds : write overflow [on dynamic memory] */
+/* test case 3 : out-of-bounds : write overflow [on dynamic memory] */
 static void write_overflow_dynmem(void)
 {
 	char *dest, src[] = "abcd56789";
@@ -252,7 +252,7 @@ static void write_overflow_dynmem(void)
 	free(dest);
 }
 
-/* option =  2 : out-of-bounds : write overflow [on compile-time memory] */
+/* test case 2 : out-of-bounds : write overflow [on compile-time memory] */
 static void write_overflow_compilemem(void)
 {
 	int i, arr[5];
@@ -264,7 +264,7 @@ static void write_overflow_compilemem(void)
 	}
 }
 
-/* option =  1 : uninitialized var test case */
+/* test case 1 : uninitialized var test case */
 static void uninit_var()
 {
 	int x;       /* static mem */
@@ -290,20 +290,20 @@ static void uninit_var()
 static void usage(char *name)
 {
 	fprintf(stderr,
-		"Usage: %s option [ -h | --help]\n"
-		" option =  1 : uninitialized var test case\n"
-		" option =  2 : out-of-bounds : write overflow [on compile-time memory]\n"
-		" option =  3 : out-of-bounds : write overflow [on dynamic memory]\n"
-		" option =  4 : out-of-bounds : write underflow\n"
-		" option =  5 : out-of-bounds : read overflow [on compile-time memory]\n"
-		" option =  6 : out-of-bounds : read overflow [on dynamic memory]\n"
-		" option =  7 : out-of-bounds : read underflow\n"
-		" option =  8 : UAF (use-after-free) test case\n"
-		" option =  9 : UAR (use-after-return) test case\n"
-		" option = 10 : double-free test case\n"
-		" option = 11 : memory leak test case 1: simple leak\n"
-		" option = 12 : memory leak test case 2: leak more (in a loop)\n"
-		" option = 13 : memory leak test case 3: \"lib\" API leak\n"
+		"Usage: %s test_case [ -h | --help]\n"
+		" test case  1 : uninitialized var test case\n"
+		" test case  2 : out-of-bounds : write overflow [on compile-time memory]\n"
+		" test case  3 : out-of-bounds : write overflow [on dynamic memory]\n"
+		" test case  4 : out-of-bounds : write underflow\n"
+		" test case  5 : out-of-bounds : read overflow [on compile-time memory]\n"
+		" test case  6 : out-of-bounds : read overflow [on dynamic memory]\n"
+		" test case  7 : out-of-bounds : read underflow\n"
+		" test case  8 : UAF (use-after-free) test case\n"
+		" test case  9 : UAR (use-after-return) test case\n"
+		" test case 10 : double-free test case\n"
+		" test case 11 : memory leak test case 1: simple leak\n"
+		" test case 12 : memory leak test case 2: leak more (in a loop)\n"
+		" test case 13 : memory leak test case 3: \"lib\" API leak\n"
 		"-h | --help : show this help screen\n", name);
 }
 
