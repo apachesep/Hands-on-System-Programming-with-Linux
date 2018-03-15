696df046393c65757f8c584003a5cf4d2d81f6ce
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2018-03-15 19:05:53 +0530

    Initial Commit for fork1 to fork4 C programs and their Makefile

---
+++ ch8/fork/fork2.c
@@ -0,0 +1,32 @@
+/*
+ * ch8:fork2.c
+ * 
+ ***************************************************************
+ * This program is part of the source code released for the book
+ *  "Hands-on System Programming with Linux"
+ *  (c) Author: Kaiwan N Billimoria
+ *  Publisher:  Packt
+ *
+ * From:
+ *  Ch 8 : Process Execution and Creation
+ ****************************************************************
+ * Brief Description:
+ * A very quick and simple demo of the fork(2) system call.
+ */
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include "../../common.h"
+
+int main(int argc, char **argv)
+{
+	if (fork() == -1)
+		FATAL("fork failed!\n");
+
+	printf("Hello, fork.\n");
+	exit(EXIT_SUCCESS);
+}
+
+/* vi: ts=8 */
