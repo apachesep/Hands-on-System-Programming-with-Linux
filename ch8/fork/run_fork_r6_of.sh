eb4ff4df70edc01eeece5d53a5750e596c4f5d1e
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2018-03-19 11:53:36 +0530

    Minor improvements

--- ch8/fork/run_fork_r6_of.sh
+++ ch8/fork/run_fork_r6_of.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# run_fork_r6_of
-
+# run_fork_r6_of.sh : simple wrapper over fork_r6_of
+FILE=tst
 rm -f fork_r6_of
 make fork_r6_of_dbg || exit 1
-valgrind ./fork_r6_of_dbg tst
-ls -l tst
-echo "Examine the file 'tst'"
+./fork_r6_of_dbg ${FILE}
+# valgrind ./fork_r6_of_dbg tst
+#   -to check with valgrind
+ls -l ${FILE}
+echo "Examine the file '${FILE}'"
