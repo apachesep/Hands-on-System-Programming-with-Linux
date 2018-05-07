d61a3e2f66f0414de4478699bdb83a7a81ee8eeb
kaiwan nb <kaiwan.billimoria@gmail.com>
2018-05-07 10:00:25 +0530

    Minor (pretty-fying)

--- common.h
+++ common.h
@@ -30,13 +30,13 @@
 #define NON_FATAL    0
 int handle_err(int fatal, const char *fmt, ...);
 
-#define WARN(warnmsg, args...) do {                   \
+#define WARN(warnmsg, args...) do {                           \
 	handle_err(NON_FATAL, "!WARNING! %s:%s:%d: " warnmsg, \
-	   __FILE__, __FUNCTION__, __LINE__, ##args); \
+	   __FILE__, __FUNCTION__, __LINE__, ##args);         \
 } while(0)
-#define FATAL(errmsg, args...) do {                   \
-	handle_err(EXIT_FAILURE, "FATAL:%s:%s:%d: " errmsg, \
-	   __FILE__, __FUNCTION__, __LINE__, ##args); \
+#define FATAL(errmsg, args...) do {                           \
+	handle_err(EXIT_FAILURE, "FATAL:%s:%s:%d: " errmsg,   \
+	   __FILE__, __FUNCTION__, __LINE__, ##args);         \
 } while(0)
 
 /*------------------------ DELAY_LOOP --------------------------------*/
