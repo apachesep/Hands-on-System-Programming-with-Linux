f2052ac9c90baf4fafef656c9a71c2ebcfaed2a0
kaiwan nb <kaiwan.billimoria@gmail.com>
2018-08-09 13:04:45 +0530

    ch15/fork_test.c : Initial Commit

---
+++ ch15/speed_multiprcs_vs_multithrd_simple/create_destroy/fork_test.c
@@ -0,0 +1,47 @@
+/*
+ * ch15:fork_test.c
+ * 
+ ***************************************************************
+ * This program is part of the source code released for the book
+ *  "Hands-on System Programming with Linux"
+ *  (c) Author: Kaiwan N Billimoria
+ *  Publisher:  Packt
+ *
+ * From:
+ *  Ch 15 : Multithreading Part I - The Essentials
+ ****************************************************************
+ * Brief Description:
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "../../../common.h"
+
+#define NFORKS 60000
+
+void do_nothing(void)
+{
+	unsigned long f = 0xb00da;
+}
+
+int main(void)
+{
+	int pid, j, status;
+
+	for (j = 0; j < NFORKS; j++) {
+		switch (pid = fork()) {
+		case -1:
+			FATAL("fork failed! [%d]\n", pid);
+		case 0:	// Child process
+			do_nothing();
+			exit(EXIT_SUCCESS);
+		default:	// Parent process
+			waitpid(pid, &status, 0);
+		}
+	}
+	exit(EXIT_SUCCESS);
+}
+
+/* vi: ts=8 */
