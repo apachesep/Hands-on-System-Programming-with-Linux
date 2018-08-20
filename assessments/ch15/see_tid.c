eac10c5d64b85144b5dcf1a47dc1dcdeea7b52c3
kaiwan nb <kaiwan.billimoria@gmail.com>
2018-08-20 12:41:39 +0530

    Initial Commit

--- assessments/ch15/see_tid.c
+++ assessments/ch15/see_tid.c
@@ -29,14 +29,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <sys/syscall.h>  /* Required for syscall(2) */
+#include <sys/syscall.h>	/* Required for syscall(2) */
 #include "../../common.h"
 
-void * worker(void *data)
+void *worker(void *data)
 {
 	long datum = (long)data;
 	printf("Worker thread #%5ld [PID %d * TID %ld *]: pausing now...\n",
-		datum, getpid(), syscall(SYS_gettid));
+	       datum, getpid(), syscall(SYS_gettid));
 	(void)pause();
 	printf(" #%ld: work done, exiting now\n", datum);
 	pthread_exit(NULL);
@@ -47,23 +47,26 @@ int main(int argc, char **argv)
 	long i;
 	int ret;
 	pthread_t tid;
-	long numthrds=0;
+	long numthrds = 0;
 
 	if (argc != 2) {
-		fprintf(stderr, "Usage: %s number-of-threads-to-create\n", argv[0]);
+		fprintf(stderr, "Usage: %s number-of-threads-to-create\n",
+			argv[0]);
 		exit(EXIT_FAILURE);
 	}
 	numthrds = atol(argv[1]);
 	if (numthrds <= 0) {
-		fprintf(stderr, "Usage: %s number-of-threads-to-create\n", argv[0]);
+		fprintf(stderr, "Usage: %s number-of-threads-to-create\n",
+			argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
 	for (i = 0; i < numthrds; i++) {
 		ret = pthread_create(&tid, NULL, worker, (void *)i);
 		if (ret)
-			FATAL("pthread_create() PID %d failed @ thread #%d! [ret=%d]\n",
-				getpid(), i, ret);
+			FATAL
+			    ("pthread_create() PID %d failed @ thread #%d! [ret=%d]\n",
+			     getpid(), i, ret);
 	}
 	pthread_exit(NULL);
 }
